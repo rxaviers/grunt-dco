bc1a7ed727dcecec12ce4d961c638b3a35cd46fa
Rafael Xavier de Souza <rxaviers@gmail.com>
2014-09-19 18:30:57 -0300

    Minor s/'/" replacement

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
 
   // Actually load this plugin's task(s).
   grunt.loadTasks("tasks");
