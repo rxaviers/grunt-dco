91dbe775222643852ebd4035f75fd0a355597f94
Rafael Xavier de Souza <rxaviers@gmail.com>
2014-09-19 19:45:10 -0300

    Default committish = HEAD (default branch) instead of master

--- tasks/dco.js
+++ tasks/dco.js
@@ -16,6 +16,7 @@ module.exports = function(grunt) {
 
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
+      committish: "HEAD",
       path: "."
     });
 
